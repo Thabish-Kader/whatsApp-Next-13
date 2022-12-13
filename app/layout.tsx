d40296739152a1e1cf4786485472cb1a62cb16b6
Thabish <kadertabish2@gmail.com>
2022-12-13 16:11:28 +0400

    header
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import { Header } from "./HEader";
 export default function RootLayout({
 	children,
 }: {
@@ -7,7 +8,10 @@ export default function RootLayout({
 	return (
 		<html>
 			<head />
-			<body>{children}</body>
+			<body>
+				<Header />
+				{children}
+			</body>
 		</html>
 	);
 }
