f579cf5d09988ee61c517e1f4ddd955729bc5571
Thabish <kadertabish2@gmail.com>
2022-12-16 18:28:22 +0400

    singlecomponent-added
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/SingleMessage.tsx
+++ app/SingleMessage.tsx
@@ -6,21 +6,33 @@ type Props = {
 };
 
 export const SingleMessage = ({ msg }: Props) => {
+	const isUser = true;
 	return (
-		<div className="flex space-x-2 w-fit">
-			<div>
-				<Image
-					src={msg?.profilePic}
-					alt={msg?.username}
-					height={50}
-					width={50}
-					className="object-contain rounded-full space-y-2 flex-shrink-0"
-				/>
-			</div>
+		<div className={`flex space-x-2 w-fit ${isUser && "ml-auto"}`}>
+			<Image
+				src={msg?.profilePic}
+				alt={msg?.username}
+				height={50}
+				width={50}
+				className={`object-contain rounded-full space-y-2 flex-shrink-0 mx-2 ${
+					isUser && "order-3"
+				}`}
+			/>
 
-			<div className="flex flex-col">
-				<p className="text-xs text-blue-400">{msg?.username}</p>
-				<p className="text-lg bg-blue-400 p-2 rounded-lg text-white">
+			<div className={`flex flex-col ${isUser && "order-2"}`}>
+				<p
+					className={`text-xs ${
+						isUser ? "text-right text-green-400" : "text-blue-400"
+					}`}
+				>
+					{msg?.username}
+				</p>
+
+				<p
+					className={`text-lg p-2 rounded-lg text-white ${
+						isUser ? "bg-green-400" : "bg-blue-400"
+					}`}
+				>
 					{msg?.message}
 				</p>
 			</div>
