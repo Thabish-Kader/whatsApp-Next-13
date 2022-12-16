be4ae6e88e15ab384e4c277bc5ea48625b96e002
Thabish <kadertabish2@gmail.com>
2022-12-16 18:12:50 +0400

    single-component-ui
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/SingleMessage.tsx
+++ app/SingleMessage.tsx
@@ -7,22 +7,26 @@ type Props = {
 
 export const SingleMessage = ({ msg }: Props) => {
 	return (
-		<div className="flex  ">
+		<div className="flex space-x-2 w-fit">
 			<div>
 				<Image
 					src={msg?.profilePic}
 					alt={msg?.username}
 					height={50}
 					width={50}
-					className="object-contain rounded-full space-y-2"
+					className="object-contain rounded-full space-y-2 flex-shrink-0"
 				/>
 			</div>
 
 			<div className="flex flex-col">
-				<p>{msg?.username}</p>
-				<p>{msg?.message}</p>
+				<p className="text-xs text-blue-400">{msg?.username}</p>
+				<p className="text-lg bg-blue-400 p-2 rounded-lg text-white">
+					{msg?.message}
+				</p>
 			</div>
-			<p className="mx-2">{new Date(msg?.created_at).toLocaleString()}</p>
+			<p className="text-xs text-gray-400">
+				{new Date(msg?.created_at).toLocaleString()}
+			</p>
 		</div>
 	);
 };
